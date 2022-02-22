c6c7b6a5ec75024d57ca007f07f6746f3c9d8678
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-02-22 16:50:44 +0000

    add class instead change style

--- main.js
+++ main.js
@@ -6,16 +6,13 @@ let hambMenu = document.querySelector('.hamburger-menu');
 
 hambBtnOpen.addEventListener('click', function() {
 	
-    hambMenu.style.display = 'block';
-
+    // hambMenu.style.display = 'block';
+    hambMenu.classList.add('active');
 })
 
 hambBtnClose.addEventListener('click', function() {
 	
-    hambMenu.style.display = 'none';
-
+    // hambMenu.style.display = 'none';
+    hambMenu.classList.remove('active');
 })
-
-
-
 
